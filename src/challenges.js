ee0ce9a5353fd0749d28a596e35941dbea1fcea5
Mixchelle <michelle.biologia@hotmail.com>
2022-08-24 16:56:12 -0300

    finalizando desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -1,72 +1,73 @@
 // Desafio 1
 function compareTrue(calor, sol) {
-if (calor && sol === true){
-  return true;
-} return false;
- }
-console.log(compareTrue(true, true))
-// Desafio 2
-function calcArea(base, heigth){
-  let area =  (base * heigth) / 2.
-  return area
-  // seu código aqui
-}
-console.log(calcArea (10, 50))
-
-// Desafio 3
-function splitSentence(frase) {
-return frase.split(' ')
-}
-console.log('foguete')
-
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
-
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
-
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
-
-// Desafio 7
-function catAndMouse(x, y, z) {
- 
-}
-
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
-
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
-
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
-
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-  techList,
-};
+  if (calor && sol === true){
+    return true;
+  } return false;
+   }
+  console.log(compareTrue(true, true))
+  // Desafio 2
+  function calcArea(base, heigth){
+    let area =  (base * heigth) / 2.
+    return area
+    // seu código aqui
+  }
+  console.log(calcArea (10, 50))
+  
+  // Desafio 3
+  function splitSentence(frase) {
+  return frase.split(' ')
+  }
+  console.log('foguete')
+  
+  // Desafio 4
+  function concatName(array) {
+    return array[array.length-1].concat(', '+array[0]);
+    // seu código aqui
+  }
+  console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']))
+  // Desafio 5
+  function footballPoints() {
+    // seu código aqui
+  }
+  
+  // Desafio 6
+  function highestCount() {
+    // seu código aqui
+  }
+  
+  // Desafio 7
+  function catAndMouse(x, y, z) {
+   
+  }
+  
+  // Desafio 8
+  function fizzBuzz() {
+    // seu código aqui
+  }
+  
+  // Desafio 9
+  function encode() {
+    // seu código aqui
+  }
+  function decode() {
+    // seu código aqui
+  }
+  
+  // Desafio 10
+  function techList() {
+    // seu código aqui
+  }
+  
+  module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    highestCount,
+    splitSentence,
+    techList,
+  };
